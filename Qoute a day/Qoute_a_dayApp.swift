7ed2a045ded2fa50346d28e3672d1a93725ff35b
Rehman Nahimi <rehman2000@live.com>
2023-11-25 13:33:07 +0000

    Initial Commit

---
+++ Qoute a day/Qoute_a_dayApp.swift
@@ -0,0 +1,17 @@
+//
+//  Qoute_a_dayApp.swift
+//  Qoute a day
+//
+//  Created by Ray Nahimi on 25/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct Qoute_a_dayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
