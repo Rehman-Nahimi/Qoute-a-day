08b60a3b2e5892bf1fce014a61858d64ef160cfa
Rehman Nahimi <rehman2000@live.com>
2023-11-25 13:41:23 +0000

    starting off the main content view screen

--- Qoute a day/ContentView.swift
+++ Qoute a day/ContentView.swift
@@ -9,13 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView{
+            VStack {
+                
+                Text("This is where the qoute will go")
+                    .font(.system(size: 30))
+                    .padding()
+                Text("Author")
+                    .font(.system(size: 18))
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
